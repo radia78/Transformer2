84f8caf901ab28013b8add455196967d8c4afe18
radia78 <radia@umich.edu>
2023-04-26 13:48:13 -0400

    Initial commit

---
+++ train.py
@@ -0,0 +1,152 @@
+import torch
+from torch.utils.tensorboard import SummaryWriter
+from transformer import *
+from utils import *
+import argparse
+from datetime import datetime
+from torch.optim import Adam
+from torch.nn import CrossEntropyLoss
+from torch.optim.lr_scheduler import LinearLR
+from torch.utils.data import DataLoader
+
+# Parse argument
+parser = argparse.ArgumentParser(description='Easier implementation of the Meta MBart')
+parser.add_argument('--batch-size', '-b', type=int, default=32)
+parser.add_argument('--epochs', '-e', type=int, default=1000)
+args = parser.parse_args()
+
+print(args.epochs)
+
+# Setup
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# Optimizer parameters
+LR = 0.001
+BETAS = (0.9, 0.98)
+EPS = 1e-6
+
+# Model hyperparameters pre-defined
+DMODEL = 512
+FF_SIZE = 2048
+NHEADS = 8
+NLAYERS = 6
+DROPOUT = 0.1
+MAXLEN = 256
+
+# Vocab Information
+def text_transforms(tensor, maxlen):
+    transforms = T.Sequential(
+        T.Truncate(maxlen),
+        T.ToTensor() 
+        )
+    return transforms(tensor)
+
+pretrain_dataset = MiniBartPreTraining('', 'pretraining_texts.pkl')
+
+# Get vocabulary information
+BOS_IDX = 0
+PAD_IDX = 1
+EOS_IDX = 2
+UNK_IDX = 3
+VOCAB_SIZE = len(pretrain_dataset.vocab.vocab)
+
+def collate_fn(batch):
+    src_batch, tgt_batch = [], []
+    for src_sample, tgt_sample in batch:
+        src_batch.append(text_transforms(src_sample, MAXLEN))
+        tgt_batch.append(text_transforms(tgt_sample, MAXLEN))
+
+    src_batch = pad_sequence(src_batch, padding_value=PAD_IDX)
+    tgt_batch = pad_sequence(tgt_batch, padding_value=PAD_IDX)
+
+pretrain_dataloader = DataLoader(pretrain_dataset, batch_size=args.batch_size, shuffle=True)
+
+model = Seq2SeqTransformer(
+    VOCAB_SIZE,
+    MAXLEN,
+    DMODEL,
+    FF_SIZE,
+    NLAYERS,
+    NHEADS,
+    DROPOUT,
+    PAD_IDX,
+    DEVICE
+    ).to(DEVICE)
+
+optimizer = Adam(model.parameters(), LR, BETAS, EPS)
+lr_scheduler = LinearLR(optimizer, 0.1, 0)
+criterion = CrossEntropyLoss()
+
+def TrainingStep(
+        epoch_number,
+        tb_writer,
+        model,
+        optimizer,
+        scheduler,
+        criterion,
+        train_dataloader
+        ):
+    
+    running_loss = 0.
+    last_loss = 0.
+
+    # Iterate through all the training batches
+    for i, data in enumerate(train_dataloader):
+        # Dims of the tokens (Batch Size, Seq Len)
+        src, tgt = data
+        src = src.to(DEVICE)
+        tgt = tgt.to(DEVICE)
+
+        # Reset the gradients
+        optimizer.zero_grad()
+
+        # Forward propagation
+        output = model(src, tgt) # (Seq Len, Batch Size, Target Vocab Size)
+        output = output.reshape(-1, output.shape[2]) # (Seq Len * Batch Size, Target Vocab Size)
+        target = tgt.reshape(-1) # (Seq Len * Batch Size, Target Vocab Size)
+
+        # Compute the loss of the model
+        loss = criterion(output, target)
+        loss.backward()
+        nn.utils.clip_grad_norm_(model.parameters(), max_norm=1)
+
+        # Gradient descent & scheduler learning rate update
+        optimizer.step()
+        scheduler.step()
+
+        running_loss += loss.item()
+        if i % 1000 == 999:
+            last_loss = running_loss/1000
+            print(f'    batch {i + 1} loss: {last_loss}')
+            tb_x = epoch_number * len(train_dataloader) + i + 1
+            tb_writer.add_scaler('Loss/train', last_loss, tb_x)
+            running_loss = 0.
+    return last_loss
+
+if __name__ == "__main__":
+    timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
+    writer = SummaryWriter(f'runs/loss_plot_{timestamp}')
+
+    # Turn on regularization for training
+    model.train(True)
+    
+    for epoch in range(args.epochs):
+        print(f'EPOCH {epoch + 1}:')
+        avg_loss = TrainingStep(
+            epoch,
+            writer,
+            model,
+            optimizer,
+            lr_scheduler,
+            criterion,
+            pretrain_dataloader
+            )
+        
+        writer.flush()
+        if (epoch + 1) % 10 == 0:
+            checkpoint = {
+                "state_dict": model.state_dict(),
+                "optimizer": optimizer.state_dict()
+                }
+            
+            save_checkpoint(checkpoint) # Save Checkpoint
---
+++ transformer.py
@@ -0,0 +1,107 @@
+import torch
+from torch import nn
+import math
+
+'''
+Author: Radi Akbar
+Date: 4/11/2023
+'''
+
+class Seq2SeqTransformer(nn.Module):
+    def __init__(self,
+                 vocab_size:int,
+                 maxlen:int,
+                 dmodel:int,
+                 ff_size:int,
+                 nlayer:int,
+                 nhead:int,
+                 dropout:float,
+                 pad_idx:int,
+                 device
+                 ):
+        super(Seq2SeqTransformer, self).__init__()
+
+        self.device = device
+        self.pad_idx = pad_idx
+
+        # Embedding layers
+        self.emb_pos = PositionalEncoding(dmodel, dropout, maxlen)
+        self.src_embedding = nn.Embedding(vocab_size, dmodel)
+        self.tgt_embedding = nn.Embedding(vocab_size, dmodel)
+
+        # Transformer layer
+        self.transformer = nn.Transformer(
+            d_model=dmodel, 
+            nhead=nhead, 
+            num_encoder_layers=nlayer,
+            num_decoder_layers=nlayer,
+            dim_feedforward=ff_size,
+            dropout=dropout,
+            activation='gelu',
+            norm_first=True
+        )
+        self.final_linear = nn.Linear(dmodel, vocab_size)
+        self._init_weights()
+
+    def _init_weights(self):
+        for p in self.parameters():
+            if p.dim() > 1:
+                nn.init.xavier_uniform_(p)
+
+    def make_padding_masks(self, src, tgt):
+        src_padding_mask = (src == self.pad_idx).transpose(0, 1) # (N, src_len)
+        src_padding_mask = torch.where(src_padding_mask, torch.tensor(float('-inf')), torch.tensor(0.))
+
+        tgt_padding_mask = (tgt == self.pad_idx).transpose(0, 1) # (N, tgt_len)
+        tgt_padding_mask = torch.where(tgt_padding_mask, torch.tensor(float('-inf')), torch.tensor(0.))
+
+        return src_padding_mask.to(self.device), tgt_padding_mask.to(self.device)
+    
+    def forward(self, src, tgt):
+
+        # Create the masks
+        src_padding_mask, tgt_padding_mask = self.make_padding_masks(src, tgt)
+        peaking_mask = self.transformer.generate_square_subsequent_mask(tgt.shape[0], self.device)
+
+        # Transformer block
+        outputs = self.final_linear(
+            self.transformer(
+                src=src,
+                tgt=tgt,
+                tgt_mask=peaking_mask,
+                src_key_padding_mask=src_padding_mask,
+                tgt_key_padding_mask=tgt_padding_mask
+            )
+        )
+        
+        return outputs
+ 
+class PositionalEncoding(nn.Module):
+    '''
+    Args:
+        maxlen: Maximum length of the sequence
+        dmodel: Dimension of the embedding
+        dropout: Dropout rate after adding the word embedding and position embedding
+    '''
+    def __init__(self, dmodel:int, dropout: float, maxlen:int=5000):
+        super(PositionalEncoding, self).__init__()
+        den = torch.exp(- torch.arange(0, dmodel, 2)* math.log(10000) / dmodel)
+        pos = torch.arange(0, maxlen).reshape(maxlen, 1)
+        pos_embedding = torch.zeros((maxlen, dmodel))
+        pos_embedding[:, 0::2] = torch.sin(pos * den)
+        pos_embedding[:, 1::2] = torch.cos(pos * den)
+        pos_embedding = pos_embedding.unsqueeze(-2)
+
+        self.dropout = nn.Dropout(dropout)
+        self.register_buffer('pos_embedding', pos_embedding)
+
+    def forward(self, token_embedding:torch.Tensor):
+        '''
+        Args:
+            token_embedding: The embedded tensor of tokens (Seq Len, Batch Size, Embedding Size)
+            
+        Returns:
+            x: The word embedding + positional encoding and applied dropout (Seq Len, Batch Size, Embedding Size)
+        '''
+        x = self.dropout(token_embedding + self.pos_embedding[:token_embedding.size(0), :])
+        return x
