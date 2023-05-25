459386873a9bdd25e78c8f478641e6bcb6048c75
Radi Akbar <radia@Radis-MacBook-Pro.local>
2023-05-25 01:02:32 -0400

    Update the scripts for the data

--- data/bookcorpus.py
+++ data/bookcorpus.py
@@ -1,78 +1,59 @@
-import os
-from tqdm import tqdm
-import numpy as np
+import huggingface_hub
 import multiprocessing
 from itertools import chain
 from datasets import load_dataset # huggingface datasets
 from transformers import BartTokenizerFast
+from huggingface_hub import HfApi
+
+# logging into huggingfacehub using the access tokens
+access_tokens = "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"
+huggingface_hub.login(access_tokens)
+
+# get the user id for this run
+user_id = HfApi().whoami()["name"]
+print(f"user id '{user_id}' will be used during the example")
 
 # number of workers in .map() call
 # good number to use is ~order number of cpu cores // 2
-num_proc = multiprocessing.count() // 2
+num_proc = multiprocessing.cpu_count() // 2
 
-# takes 54GB in huggingface .cache dir, about 8M documents (8,013,769)
-dataset = load_dataset('wikipedia', '20220301.en')
+# load the bookcorpus dataset
+dataset = load_dataset('bookcorpus')
 
-# owt by default only contains the 'train' split, so create a test split
+# split the data into train split
 split_dataset = dataset['train'].train_test_split(test_size=0.0005, seed=2357, shuffle=True)
-split_dataset['val'] = split_dataset.pop('test') # rename the test split to val
+split_dataset['train_bookcorpus'] = split_dataset.pop('train') # rename the train split to train_bookcorpus
+split_dataset['val_bookcorpus'] = split_dataset.pop('test') # rename the test split to val_bookcorpus
 
-# this results in:
-# >>> split_dataset
-# DatasetDict({
-#     train: Dataset({
-#         features: ['text'],
-#         num_rows: 8009762
-#     })
-#     val: Dataset({
-#         features: ['text'],
-#         num_rows: 4007
-#     })
-# })
+# load the bart tokenizer from facebook
+enc = BartTokenizerFast.from_pretrained('facebook/bart-base')
 
-enc = BartTokenizerFast.from_pretrained('facebook/bart-base', max_length=512) # load the bart-base tokenizer
+# define the preprocessing functions˙
 def process(examples):
-    concat_examples = {k: list(chain(*examples[k])) for k in examples.keys()}
-    tot_len = len(concat_examples[list(examples.keys())[0]])
+    return enc(
+       examples["text"], return_special_tokens_mask=True, truncation=True, max_length=512
+    )
 
-    if tot_len >= enc.model_max_length:
-        tot_len = (tot_len // enc.model_max_length) * enc.model_max_length
-    
+def group_texts(examples):
+    # Concatenate all texts.
+    concatenated_examples = {k: list(chain(*examples[k])) for k in examples.keys()}
+    total_length = len(concatenated_examples[list(examples.keys())[0]])
+    # We drop the small remainder, we could add padding if the model supported it instead of this drop, you can
+    # customize this part to your needs.
+    if total_length >= enc.model_max_length:
+        total_length = (total_length // 512) * 512
+    # Split by chunks of max_len.
     result = {
-        k: [t[i : i + enc.model_max_length] for i in range(0, tot_len, enc.model_max_length)]
-        for k, t in concat_examples.items()
+        k: [t[i : i + 512] for i in range(0, total_length, 512)]
+        for k, t in concatenated_examples.items()
     }
     return result
 
-# tokenize the dataset
-tokenized = split_dataset.map(
-    process,
-    remove_columns=['text'],
-    desc="concatenating and splitting the data into chunks",
-    num_proc=num_proc,
-)
+# apply the process to the datasets and shuffle
+tokenized_datasets = split_dataset.map(process, batched=True, remove_columns=["text"], num_proc=num_proc)
+tokenized_datasets = tokenized_datasets.map(group_texts, batched=True, num_proc=num_proc)
+tokenized_datasets = tokenized_datasets.shuffle(seed=34)
 
-# concatenate all the ids in each dataset into one large file we can use for training
-for split, dset in tokenized.items():
-    arr_len = np.sum(dset['len'])
-    filename = os.path.join(os.path.dirname(__file__), f'{split}.bin')
-    dtype = np.uint16 # (can do since enc.max_token_value == 50256 is < 2**16)
-    arr = np.memmap(filename, dtype=dtype, mode='w+', shape=(arr_len,))
-    total_batches = 1024
-
-    idx = 0
-    for batch_idx in tqdm(range(total_batches), desc=f'writing {filename}'):
-        # Batch together samples for faster write
-        batch = dset.shard(num_shards=total_batches, index=batch_idx, contiguous=True).with_format('numpy')
-        arr_batch = np.concatenate(batch['ids'])
-        # Write into mmap
-        arr[idx : idx + len(arr_batch)] = arr_batch
-        idx += len(arr_batch)
-    arr.flush()
-
-# train.bin is ~17GB, val.bin ~8.5MB
-# train has ~9B tokens (9,035,582,198)
-# val has ~4M tokens (4,434,897)
-
-# to read the bin files later, e.g. with numpy:
-# m = np.memmap('train.bin', dtype=np.uint16, mode='r')
+# push the preprocess data into the hub
+dataset_id = f"{user_id}\processed_bart_data"
+tokenized_datasets.push_to_hub("processed_bart_data")
