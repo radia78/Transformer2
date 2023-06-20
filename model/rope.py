d5f8cb25a566def57463b37dd37174e64fd2445a
Radi Akbar <radia@Radis-MacBook-Pro.local>
2023-06-19 22:07:25 -0400

    Updated rotary positional embedding with a unit test

--- model/rope.py
+++ model/rope.py
@@ -1,4 +1,6 @@
 import torch
+import unittest
+import numpy as np
 from einops import repeat
 
 class Rotary(torch.nn.Module):
@@ -29,3 +31,54 @@ def rotate_every_two(x):
 @torch.jit.script
 def apply_rotary_pos_emb(x, cos, sin):
     return (x * cos) + (rotate_every_two(x) * sin)
+
+# define a unittest for the positional embedding layer
+class ROPETestCase(unittest.TestCase):
+    def test_forward(self):
+        # define the test case 1: [1, 0], [1, 0]
+        test_input1 = torch.tensor([[1, 0], [0, 1]]).float()
+
+        # compute using the positional embedding
+        rope1 = Rotary(test_input1.shape[-1])
+        cos1, sin1 = rope1(test_input1.unsqueeze(0).unsqueeze(1), seq_dim=2)
+        module_output1 = apply_rotary_pos_emb(test_input1, cos1, sin1)
+
+        # compute using rotational matrix
+        expected_output1 = torch.cat(
+            [rotate_matrix(test_input1[i].reshape(1, 2), i) for i in range(test_input1.shape[0])]
+        )
+
+        # test case 1
+        self.assertTrue(torch.allclose(module_output1, expected_output1)), "Test 1 Failed: Vector is not rotated by theta1"
+
+        # define the test case 2: random input of length 10
+        test_input2 = torch.randn(10, 2)
+
+        # compute using positional embedding
+        rope2 = Rotary(test_input2.shape[-1])
+        cos2, sin2 = rope2(test_input2.unsqueeze(0).unsqueeze(1), seq_dim=2)
+        module_output2 = apply_rotary_pos_emb(test_input2, cos2, sin2)
+
+        # compute using rotational matrix
+        expected_output2 = torch.cat(
+            [rotate_matrix(test_input2[i].reshape(1, 2), i) for i in range(test_input2.shape[0])]
+        )
+
+        # test case 2
+        self.assertTrue(torch.allclose(module_output2, expected_output2)), "Test 2 Failed: Vector is not rotated by theta1"
+        
+
+def rotate_matrix(x, m:int):
+    """Rotate a vector by theta 1 counter clockwise (theta 1 is the angle of the first 2 embeddings)"""
+    assert x.shape == torch.Size([1, 2]), "'x' is not a 2-length vector"
+
+    # compute components
+    theta = 10000**(-2*(1 - 1)/2)
+    sin_comp = np.sin(m * theta)
+    cos_comp = np.cos(m * theta)
+    rotation_matrix = torch.tensor([[cos_comp, sin_comp], [-sin_comp, cos_comp]]).float()
+
+    return torch.matmul(x, rotation_matrix)
+
+if __name__ == "__main__":
+    unittest.main()
