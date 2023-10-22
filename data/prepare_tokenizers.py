97916c0101f748c9db1b99e3b9dc37e4ff0720b2
Radi Akbar <radia@Radis-MacBook-Pro.local>
2023-10-22 17:29:02 -0400

    replacing the datasets

--- data/prepare_tokenizers.py
+++ data/prepare_tokenizers.py
@@ -6,7 +6,7 @@ from tokenizers.processors import TemplateProcessing
 from tokenizers.decoders import BPEDecoder
 from dataclasses import dataclass, field
 from typing import List
-from datasets import inspect_dataset, load_dataset_builder
+from datasets import load_dataset
 from transformers import PreTrainedTokenizerFast
 import huggingface_hub
 from huggingface_hub import HfApi
@@ -28,7 +28,7 @@ class MachineTranslationTokenizer:
 
     def train_tokenizer(self, dataset, save_dir:str, langs:List[str]):
         tokenizer, trainer = self.prepare_tokenizer()
-        tokenizer.train_from_iterator([dataset['train'][lang] for lang in langs], trainer)
+        tokenizer.train_from_iterator([i[lang] for lang in langs for i in dataset], trainer)
         tokenizer.decoder = BPEDecoder(suffix="_")
         tokenizer.enable_truncation(max_length=512)
         tokenizer.post_processor = TemplateProcessing(
@@ -47,30 +47,12 @@ class TokenizerConfig:
 if __name__ == "__main__":
     train_tokenizer = True # train the tokenizer and push it to huggingface
 
-    inspect_dataset("wmt14", "path/to/scripts")
-    builder = load_dataset_builder(
-        "path/to/scripts/wmt_utils.py",
-        language_pair=("de", "en"),
-        subsets={
-            datasets.Split.TRAIN: ["commoncrawl_deen"],
-            datasets.Split.VALIDATION: ["euelections_dev2019"],
-        },
-    )
-
-# Standard version
-builder.download_and_prepare()
-ds = builder.as_dataset()
-
-# Streamable version
-ds = builder.as_streaming_dataset()
-
-
     if train_tokenizer:
         langs = ['de', 'en']
-        dataset = load_dataset("")
+        dataset = load_dataset("wmt14", f"{langs[0]}-{langs[1]}", split="train")['translation']
         tok_config = TokenizerConfig(spl_tokens=["<s>", "<pad>", "</s>", "<mask>", "<unk>"])
         tokenizer = MachineTranslationTokenizer(tok_config) # initiate class
-        tokenizer.train_tokenizer(dataset, "data/mt/transformer-mt-base", langs) # train the tokenizer
+        tokenizer.train_tokenizer(dataset, "data/transformer-mt-base", langs) # train the tokenizer
 
     # logging into huggingfacehub using the access tokens
     access_tokens = "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"
@@ -82,6 +64,6 @@ ds = builder.as_streaming_dataset()
         "pretrained_model_name_or_path": f"{user_id}/wmt14-de2en-tokenizer"
     } # declare the tokenzier config
 
-    tokenizer = PreTrainedTokenizerFast(tokenizer_file="/Users/radia/Projects/ml_projects/MiniBart/data/mt/transformer-mt-base.json")
+    tokenizer = PreTrainedTokenizerFast(tokenizer_file="/Users/radia/Projects/ml_projects/MiniBart/data/transformer-mt-base.json")
     tokenizer.push_to_hub(f"{user_id}/wmt14-de2en-tokenizer", model_tag="v1.0", tokenizer_config=tokenizer_config)
 
