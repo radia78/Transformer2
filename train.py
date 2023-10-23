e6b3c27e8e9035c7bdbf9efa8391d91946d9d3b4
Radi Akbar <radia@Radis-MacBook-Pro.local>
2023-10-23 15:41:37 -0400

    Adding torch compile in the training script

--- train.py
+++ train.py
@@ -34,6 +34,7 @@ def create_model(args):
         dropout=args.dropout
     )
     model = Transformer(model_config).to(LOCAL_RANK)
+    model = torch.compile(model, dynamic=True)
     model = DDP(model, device_ids=[LOCAL_RANK], output_device=LOCAL_RANK) # send the model across GPU/nodes
     return model
 
